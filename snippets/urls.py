820590071bc5bc0e9afbda115c3f3527d40c0f4a
BeerFairy <moodholly78@gmail.com>
2021-02-22 18:33:28 +0600

    initial commit

---
+++ snippets/urls.py
@@ -0,0 +1,14 @@
+from snippets import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+# привзка с использованием маршрутизаторов
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
